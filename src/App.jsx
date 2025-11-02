eff13e891508e7324d4ac0e56b8eeb2e79d41e43
구혁모 <gurah129@gmail.com>
2025-11-02 21:36:02 +0900

    🌱 패키지 추가 및 초기설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 모바일 레이아웃 설정 */}
+        {/* TODO 추후 인증 로직 추가 */}
+        <Route element={<Layout />}>
+          {/* <Route path="/login" element={<LoginPage />} />; */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
