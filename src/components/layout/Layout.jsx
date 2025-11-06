750d2075f4f8f290f90d2902adafb9857ec999ac
구혁모 <gurah129@gmail.com>
2025-11-06 14:00:59 +0900

    💄 모바일 레이아웃 스타일 설정

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,8 +2,15 @@ import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
-    <>
+    <div
+      style={{
+        maxWidth: '430px',
+        height: '100dvh',
+        margin: '0 auto',
+        position: 'relative',
+      }}
+    >
       <Outlet />
-    </>
+    </div>
   );
 };
