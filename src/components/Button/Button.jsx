9097d2fb60916535a0475960aee95c94fc9d226a
구혁모 <gurah129@gmail.com>
2025-11-06 15:34:17 +0900

    ✨ 버튼 컴포넌트 구현

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,18 @@
+import styles from './Button.module.scss';
+
+export const Button = ({ text = 'button', onClick, isActive = false }) => {
+  return (
+    <button
+      className={styles.container}
+      style={{
+        border: `${isActive ? '1.5px' : '2px'} solid #F56E00 inset`,
+        color: isActive ? 'white' : '#F56E00',
+        backgroundColor: isActive ? '#F56E00' : 'white',
+      }}
+      onClick={onClick}
+      disabled={!isActive}
+    >
+      {text}
+    </button>
+  );
+};
