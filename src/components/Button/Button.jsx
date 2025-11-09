2c81d64c7a74e4b4d547daa269b8f6aad1e733a3
구혁모 <gurah129@gmail.com>
2025-11-09 13:42:37 +0900

    🎨 버튼 컴포넌트 패딩 추상화

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,11 @@
 import styles from './Button.module.scss';
 
-export const Button = ({ text = 'button', onClick, isActive = false }) => {
+export const Button = ({
+  text = 'button',
+  onClick,
+  isActive = false,
+  padding = '17px 0',
+}) => {
   return (
     <button
       className={styles.container}
@@ -8,6 +13,7 @@ export const Button = ({ text = 'button', onClick, isActive = false }) => {
         border: `${isActive ? '1.5px' : '2px'} solid #F56E00`,
         color: isActive ? 'white' : '#F56E00',
         backgroundColor: isActive ? '#F56E00' : 'white',
+        padding: padding,
       }}
       onClick={onClick}
       disabled={!isActive}
