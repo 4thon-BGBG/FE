96ed2c83028d51a14693595b987436a13d28080f
구혁모 <gurah129@gmail.com>
2025-11-14 16:31:57 +0900

    🔥 토큰 로그 삭제

--- src/components/layout/ProtectedRoute.jsx
+++ src/components/layout/ProtectedRoute.jsx
@@ -1,14 +1,9 @@
 // src/components/ProtectedRoute.jsx
-import React, { useEffect } from 'react';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 
 export const ProtectedRoute = () => {
   const accessToken = localStorage.getItem('accessToken');
   const location = useLocation(); // 현재 경로 정보
-
-  useEffect(() => {
-    console.log(localStorage.getItem('accessToken'));
-  }, [accessToken]);
 
   if (!accessToken) {
     return <Navigate to="/login" state={{ from: location }} replace />;
